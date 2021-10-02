3b73ebffd15256be1d09047937f5a431a581f5a9
o3ojsk <hisunggyu@gmail.com>
2021-10-02 19:36:35 +0800

    APP-PHP-DB

--- app/src/main/java/com/example/roboticVacuum/MainActivity.java
+++ app/src/main/java/com/example/roboticVacuum/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.roboticVacuum;
 
-import static com.example.roboticVacuum.R.*;
-
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -13,13 +14,31 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.roboticVacuum.databinding.ActivityMainBinding;
+import com.example.roboticVacuum.dto.CommandDTO;
 import com.example.roboticVacuum.service.BtnEventService;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ActivityMainBinding binding;
     private final BtnEventService btnEventService = new BtnEventService();
+    private final String url = "http://192.168.0.2/test.php";
+    private ArrayList<CommandDTO> list = new ArrayList<>();
+    private TextView textView;
+    private String jsonString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,33 +47,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(id.nav_view);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                id.navigation_modeScreen, id.navigation_bluetooth, id.navigation_remocon)
+                R.id.navigation_modeScreen, R.id.navigation_bluetooth, R.id.navigation_remocon)
                 .build();
-        NavController navController = Navigation.findNavController(this, id.nav_host_fragment_activity_main);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
 
-        findViewById(id.btnRandom).setOnClickListener(this);
-        findViewById(id.btnCircle).setOnClickListener(this);
-        findViewById(id.btnWallFollowing).setOnClickListener(this);
-        findViewById(id.btnWave).setOnClickListener(this);
-        findViewById(id.btnRemoteUp).setOnClickListener(this);
-        findViewById(id.btnRemoteDown).setOnClickListener(this);
-        findViewById(id.btnRemoteRight).setOnClickListener(this);
-        findViewById(id.btnRemoteLeft).setOnClickListener(this);
+        findViewById(R.id.btnRandom).setOnClickListener(this);
+        findViewById(R.id.btnCircle).setOnClickListener(this);
+        findViewById(R.id.btnWallFollowing).setOnClickListener(this);
+        findViewById(R.id.btnWave).setOnClickListener(this);
+
+//        findViewById(R.id.btnRemoteUp).setOnClickListener(this);
+//        findViewById(R.id.btnRemoteDown).setOnClickListener(this);
+//        findViewById(R.id.btnRemoteRight).setOnClickListener(this);
+//        findViewById(R.id.btnRemoteLeft).setOnClickListener(this);
     }
 
     @Override
-    public void onClick(View v){
+    public void onClick(View v) {
         int id = v.getId();
         switch (id) {
             case R.id.btnRandom:
                 //btnRandom Click Event
-                btnEventService.pressedRandomButton((Button) findViewById(id));
+                Button btnRandom = findViewById(R.id.btnRandom);
+                textView = findViewById(R.id.textView);
+                JsonParse jsonParse = new JsonParse();      // AsyncTask 생성
+                jsonParse.execute(url);     // AsyncTask 실행
+                btnRandom.setText(String.valueOf(list.size()));
+//                btnEventService.pressedRandomButton(findViewById(id));
                 break;
             case R.id.btnCircle:
                 btnEventService.pressedCircleButton();
@@ -65,18 +90,112 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btnWave:
                 btnEventService.pressedWaveButton();
                 break;
-            case R.id.btnRemoteUp:
-                btnEventService.pressedUpButton();
-                break;
-            case R.id.btnRemoteDown:
-                btnEventService.pressedDownButton();
-                break;
-            case R.id.btnRemoteRight:
-                btnEventService.pressedRightButton();
-                break;
-            case R.id.btnRemoteLeft:
-                btnEventService.pressedLeftButton();
-                break;
+//            case R.id.btnRemoteUp:
+//                btnEventService.pressedUpButton(isRecording);
+//                break;
+//            case R.id.btnRemoteDown:
+//                btnEventService.pressedDownButton(isRecording);
+//                break;
+//            case R.id.btnRemoteRight:
+//                btnEventService.pressedRightButton(isRecording);
+//                break;
+//            case R.id.btnRemoteLeft:
+//                btnEventService.pressedLeftButton(isRecording);
+//                break;
         }
+    }
+
+    //https://1d1cblog.tistory.com/133
+    public class JsonParse extends AsyncTask<String, Void, String> {
+        private String TAG = "JsonParseTest";
+
+        @Override
+        protected String doInBackground(String... strings) { // execute 의 매개변수를 받아와서 사용
+            String url = strings[0];
+            try {
+                URL serverURL = new URL(url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) serverURL.openConnection();
+
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.connect();
+
+                int responseCode = httpURLConnection.getResponseCode();
+
+                InputStream ips;
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    ips = httpURLConnection.getInputStream();
+                } else {
+                    ips = httpURLConnection.getErrorStream();
+                }
+
+                InputStreamReader isr = new InputStreamReader(ips, StandardCharsets.UTF_8);
+                BufferedReader bufferedReader = new BufferedReader(isr);
+
+                StringBuilder sb = new StringBuilder();
+                String line;
+
+                while ((line = bufferedReader.readLine()) != null) {
+                    sb.append(line);
+                }
+
+                bufferedReader.close();
+                Log.d(TAG, sb.toString().trim());
+
+                return sb.toString().trim();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String fromDoInBackgroundString) {  // doInBackgroundString 에서 return 한 값을 받음
+            super.onPostExecute(fromDoInBackgroundString);
+
+            if (fromDoInBackgroundString == null)
+                textView.setText("error");
+            else {
+                jsonString = fromDoInBackgroundString;
+                list.clear();
+                doParse();
+                Log.d(TAG, list.get(0).getName());
+                textView.setText(list.get(0).getName());
+            }
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+        private void doParse() {
+            try {
+                JSONObject jsonObject = new JSONObject(jsonString);
+                JSONArray jsonArray = jsonObject.getJSONArray("cmd_table");
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    CommandDTO commandDTO = new CommandDTO();
+
+                    JSONObject item = jsonArray.getJSONObject(i);
+                    commandDTO.setIndex(item.getLong("index"));
+                    commandDTO.setName(item.getString("name"));
+                    commandDTO.setMove(item.getInt("move"));
+
+                    list.add(commandDTO);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            for (CommandDTO dto : list) {
+                Log.d(">>", dto.toString());
+            }
+        } // JSON을 가공하여 ArrayList에 넣음
     }
 }
---
+++ app/src/main/java/com/example/roboticVacuum/dto/CommandDTO.java
@@ -0,0 +1,64 @@
+package com.example.roboticVacuum.dto;
+
+/**
+ * command
+ * table : CMD_TABLE
+ * query
+CREATE TABLE `CMD_TABLE` (
+    `INDEX` BIGINT(20) NOT NULL AUTO_INCREMENT,
+    `NAME` VARCHAR(50) NOT NULL DEFAULT 'defualt' COLLATE 'utf8mb4_general_ci',
+    `MOVE` INT(1) NOT NULL DEFAULT '0',
+    PRIMARY KEY (`index`) USING BTREE)
+COLLATE='utf8mb4_general_ci'
+ENGINE=InnoDB;
+ * INDEX : primary key
+ * NAME : saved name
+ * MOVE : up, down, right, left == 0, 1, 2, 3
+ */
+public class CommandDTO {
+    private Long index;
+    private String name;
+    private int move;
+
+    public CommandDTO() {
+    }
+
+    public CommandDTO(Long index, String id, int move) {
+        this.index = index;
+        this.name = id;
+        this.move = move;
+    }
+
+    public Long getIndex() {
+        return index;
+    }
+
+    public void setIndex(Long index) {
+        this.index = index;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMove() {
+        return move;
+    }
+
+    public void setMove(int move) {
+        this.move = move;
+    }
+
+    @Override
+    public String toString() {
+        return "CommandDTO{" +
+                "index=" + index +
+                ", name='" + name + '\'' +
+                ", move=" + move +
+                '}';
+    }
+}
